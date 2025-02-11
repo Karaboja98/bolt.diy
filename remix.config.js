dc6e5e59f891abc565781af0cdc3a6932637d4b9
Karaboja98 <142131684+Karaboja98@users.noreply.github.com>
2025-02-12 00:29:13 +0100

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,1 +1,7 @@
-
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  serverBuildPath: "functions/[[path]].js", // Pfad zum Server-Build für Cloudflare Pages Functions
+  serverModuleFormat: "esm", // ESM ist für Cloudflare Workers empfohlen
+  // Ignoriere leere CSS-Module Warnung (optional, falls Sie CSS-Module verwenden)
+  serverDependenciesToBundle: ['@remix-run/cloudflare-pages']
+};
